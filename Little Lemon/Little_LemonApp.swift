9a676b1352f26fce41d75573a25d17a27c5fa619
Edem Ahorlu <eka13@duke.edu>
2023-12-19 04:17:38 -0500

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Edem Ahorlu on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
