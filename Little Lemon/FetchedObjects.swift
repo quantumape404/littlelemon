09ce56a32d1ac40429a61d56f04213d91b6b6105
Edem Ahorlu <eka13@duke.edu>
2023-12-20 06:48:55 -0500

    readded core data files

---
+++ Little Lemon/FetchedObjects.swift
@@ -0,0 +1,8 @@
+//
+//  FetchedObjects.swift
+//  Little Lemon
+//
+//  Created by Edem Ahorlu on 12/20/23.
+//
+
+import Foundation
---
+++ Little Lemon/Persistence.swift
@@ -0,0 +1,8 @@
+//
+//  Persistence.swift
+//  Little Lemon
+//
+//  Created by Edem Ahorlu on 12/20/23.
+//
+
+import Foundation
